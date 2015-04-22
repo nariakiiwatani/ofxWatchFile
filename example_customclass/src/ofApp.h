912e469627958e879d21d496fdc61c31f6dc6e60
nariakiiwatani <nariakiiwatani@annolab.com>
2015-04-22 14:11:23 +0900

    added examples

---
+++ example_customclass/src/ofApp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxWatchFile.h"
+
+class AutoReloadImage : public ofxWatchFile, public ofImage
+{
+private:
+	void reload(ofFile &file) {
+		loadImage(file.path());
+	}
+};
+
+class ofApp : public ofBaseApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+private:
+	AutoReloadImage file_;
+};
---
+++ example_listener/src/ofApp.cpp
@@ -0,0 +1,66 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofAddListener(file_.loadedEvent, this, &ofApp::fileEvent);
+	file_.setTargetPath("hoge.txt");
+}
+void ofApp::fileEvent(ofFile &file)
+{
+	cout << "loaded" << endl;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
